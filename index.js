3fd8a3bcb3b55e164e50e1dc257dbd0a7ea8dd3a
Beden <+>
2022-11-30 09:12:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
+  let input = document.getElementById("input");
+  input.addEventListener("addEventListener", function () {});
 }
+
+// const sinon = require("sinon");
+
+// describe("index.js", () => {
+//   let input;
+
+//   beforeEach(function () {
+//     input = document.getElementById("input");
+//     sinon.spy(input, "addEventListener");
+//   });
+
+//   it("binds an event listener in addingEventListener()", () => {
+//     addingEventListener();
+//     expect(input.addEventListener.called).to.be.true;
+//   });
+// });
